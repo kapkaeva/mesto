02e9b09995d7b5a05bb2ca520daf00d4c5d8e535
Aida Kapkaeva <aida.kapkaeva@gmail.com>
2021-02-26 16:50:45 -0800

    Adapts porperly on mobile and tablet screens

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+let popUp = document.querySelector('.popup'); 
+
+let formElement = document.querySelector('.form__edit-profile');
+let nameInput = document.querySelector('.form__text_type_name');
+let jobInput = document.querySelector('.form__text_type_description');
+
+let editButton = document.querySelector('.profile__edit-btn');
+let closeButton = document.querySelector('.popup__close-btn');
+
+let profileName = document.querySelector('.profile__heading');
+let description = document.querySelector('.profile__description');
+
+
+function editProfileInfo() {
+  closeFormProfileInfoEdit();
+  nameInput.value = profileName.textContent;
+  jobInput.value = description.textContent;
+}
+
+function closeFormProfileInfoEdit() {
+  popUp.classList.toggle('popup__closed');
+}
+
+function handleFormProfileSubmit(evt) {
+  evt.preventDefault(evt)
+  profileName.textContent = nameInput.value;
+  description.textContent = jobInput.value;
+  closeFormProfileInfoEdit();
+}
+
+editButton.addEventListener('click', editProfileInfo);
+closeButton.addEventListener('click', closeFormProfileInfoEdit);
+formElement.addEventListener('submit', handleFormProfileSubmit); 
