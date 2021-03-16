ed2e8f7ae55a723826873ff639e641e63c2892d5
kapkaeva <71045013+kapkaeva@users.noreply.github.com>
2021-03-15 21:37:35 -0700

    Feature/mesto (#1)
    * Popup close button work for all popups on page
    
    * Added like button
    
    * Added deleting card
    
    Popup opened with image and close button
    
    Added smooth opening and closing of popups
    
    Ready to review
    
    * Update mesto__no-items-text.css
    
    * Update button.css
    
    * Update mesto__trash.css
    
    * Update popup__close-btn.css
    
    * Update popup__container_bg.css
    
    * Update popup__image.css
    
    * Update popup__img-title.css
    
    * Update popup_bg.css
    
    * Update popup_hidden.css

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,31 @@
-let popUp = document.querySelector('.popup'); 
+let popupEditPofile = document.getElementById('popup__edit-profile');
 
-let formElement = document.querySelector('.form');
+let formEditProfile = document.querySelector('[name="editprofile"]');
 let nameInput = document.querySelector('.form__text_type_name');
 let jobInput = document.querySelector('.form__text_type_description');
 
 let editButton = document.querySelector('.profile__edit-btn');
-let closeButton = document.querySelector('.popup__close-btn');
+let closeButtons = document.querySelectorAll('.popup__close-btn');
 
 let profileName = document.querySelector('.profile__heading');
 let description = document.querySelector('.profile__description');
 
-let mestoImage = document.querySelector('mesto__image');
-let mestoTitle = document.querySelector('.mesto__title');
-let noMestoItem = document.querySelector('.mesto__no-items');
+let addButton = document.querySelector('.profile__add-btn');
+let popupAddMesto = document.getElementById('popup__add-mesto');
 
+let popupViewMestoImage = document.getElementById('popup__view-mesto-image');
+
+const mestoTemplate = document.getElementById('mesto__template').content;
+const mestoGrid = document.querySelector('.mesto__grid');
+
+let mestoImageInput = document.querySelector('.form__text_type_mesto-link');
+let mestoTitleInput = document.querySelector('.form__text_type_mesto-title');
+
+let noMestoItem = document.querySelector('.mesto__no-items');
+let formAddMesto = document.querySelector('[name="addmesto"]');
+
+let mestoImage = document.querySelector('.popup__image');
+let mestoTitle = document.querySelector('.popup__img-title');
 
 const initialCards = [
   {
@@ -40,53 +52,102 @@ const initialCards = [
     name: 'ТВ и радио антенна тауэр',
     link: './images/mesto/f161a9ea-d670-42ee-a77e-7b81d0a03372.jpg'
   }
-]; 
+];
+
+
+function newCardItem(name, link) {
+  const mestoElement = mestoTemplate.querySelector('.mesto__item').cloneNode(true);
+  mestoElement.querySelector('.mesto__image').src = link;
+  mestoElement.querySelector('.mesto__image').alt = name;
+  mestoElement.querySelector('.mesto__title').textContent = name;
+  addCardItemListeners(mestoElement);
+  return mestoElement;
+}
+
+function addCardItemListeners(mestoElement) {
+  listenLikeCard(mestoElement.querySelector('.mesto__like'));
+  listenTrashCard(mestoElement.querySelector('.mesto__trash'));
+  listenMestoImage(mestoElement.querySelector('.mesto__image'));
+}
 
 function showMestoCards(cards) {
-  const mestoTemplate = document.getElementById('mesto__template').content;
-  const mestoGrid = document.querySelector('.mesto__grid');
-
-  
+  noMestoItem.classList.add('mesto__no-items_hidden');
   cards.forEach(mesto => {
-    const mestoElement = mestoTemplate.querySelector('.mesto__item').cloneNode(true);
-    mestoElement.querySelector('.mesto__image').src = mesto['link'];
-    mestoElement.querySelector('.mesto__image').alt = mesto['name'];
-    mestoElement.querySelector('.mesto__title').textContent = mesto['name'];
-    mestoGrid.append(mestoElement); 
+    let card = newCardItem(mesto['name'], mesto['link']);
+    mestoGrid.append(card);
   });
 }
 
-function renderHasMesto() {
-  noMestoItem.classList.add('mesto__no-items_hidden');
-  showMestoCards(initialCards);
-
-}
-
-function renderNoMesto() {
-  noMestoItem.classList.remove('mesto__no-items_hidden');
-}
-
 function editProfileInfo() {
-  popUp.classList.add('popup_opened');
+  openPopup(popupEditPofile);
   nameInput.value = profileName.textContent;
   jobInput.value = description.textContent;
 }
 
-function closeFormProfileInfoEdit() {
-  popUp.classList.remove('popup_opened');
+function addMesto() {
+  openPopup(popupAddMesto);
+}
+
+function openPopup(element) {
+  element.classList.add('popup_opened');
+  element.classList.remove('popup_hidden');
+}
+
+function closePopup(event) {
+  let element = event.target.closest('.popup');
+  element.classList.remove('popup_opened');
+  element.classList.add('popup_hidden');
 }
 
 function handleFormProfileSubmit(evt) {
   evt.preventDefault(evt)
   profileName.textContent = nameInput.value;
   description.textContent = jobInput.value;
-  closeFormProfileInfoEdit();
+  closePopup(evt);
 }
 
+function handleFormMestoSubmit(evt) {
+  evt.preventDefault(evt)
+  let card = newCardItem(mestoTitleInput.value, mestoImageInput.value);
+  mestoGrid.insertBefore(card, mestoGrid.childNodes[0]);
+  closePopup(evt);
+}
+
+function listenLikeCard(mestoLike) {
+  mestoLike.addEventListener('click', function (element) {
+    if (mestoLike.classList.contains('mesto__like_active')) {
+      mestoLike.classList.remove('mesto__like_active');
+    } else {
+      mestoLike.classList.add('mesto__like_active');
+    }
+  });
+}
+
+function listenTrashCard(mestoTrash) {
+  mestoTrash.addEventListener('click', function (element) {
+    if (mestoTrash.parentNode) {
+      mestoTrash.parentNode.remove(mestoTrash);
+    }
+    if (document.querySelectorAll('.mesto__item').length === 0) {
+      noMestoItem.classList.remove('mesto__no-items_hidden');
+    }
+  });
+}
+
+function listenMestoImage(image) {
+  image.addEventListener('click', function (element) {
+    openPopup(popupViewMestoImage);
+    mestoImage.src = element.target.src;
+    mestoTitle.textContent = element.target.alt;
+  });
+}
+
+
 editButton.addEventListener('click', editProfileInfo);
-closeButton.addEventListener('click', closeFormProfileInfoEdit);
-formElement.addEventListener('submit', handleFormProfileSubmit); 
+addButton.addEventListener('click', addMesto);
+closeButtons.forEach(button => button.addEventListener('click', closePopup));
+formEditProfile.addEventListener('submit', handleFormProfileSubmit);
+formAddMesto.addEventListener('submit', handleFormMestoSubmit);
 
 
-renderHasMesto()
-//renderNoMesto()
+showMestoCards(initialCards)
